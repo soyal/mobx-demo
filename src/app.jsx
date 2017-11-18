65ea50fc781887ec75fae07b47be056f238521fe
Soyal <345723940@qq.com>
2017-11-18 16:11:16 +0800

    action

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react'
+import {
+  useStrict,
+  observable,
+  action,
+  runInAction
+} from 'mobx'
+import { observer } from 'mobx-react'
 
-function test(target) {
-  console.log(target)
-}
+useStrict(true)
 
-@test
+@observer
 class App extends Component {
+  @observable
+  store = {
+    count: 0,
+    test: 1
+  }
+
+  countUp() {
+    setTimeout(() => {
+      runInAction(() => {
+        this.store.count++
+        this.store.test ++
+      })
+    }, 1000)
+  }
+
   render() {
+    const { store } = this
+    console.log('render run')
     return (
-      <div className="test">todo list</div>
+      <div className="counter">
+        count: {store.count}
+        test: {store.test}
+        <div>
+          <button onClick={this.countUp.bind(this)}>count up</button>
+        </div>
+      </div>
     )
   }
 }
